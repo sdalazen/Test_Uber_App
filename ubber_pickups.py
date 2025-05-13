9ab804ee792b07cf6e5ee581b93ab400a94d2d74
sdalazen <sba22149@student.cct.ie>
2025-05-13 19:02:25 +0100

    Update

--- ubber_pickups.py
+++ ubber_pickups.py
@@ -22,3 +22,7 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
 
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
