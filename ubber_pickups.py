c0b6bdf7cebf495c0716d09146e4c1f4ed0114d5
sdalazen <sba22149@student.cct.ie>
2025-05-13 19:04:34 +0100

    Update

--- ubber_pickups.py
+++ ubber_pickups.py
@@ -30,3 +30,6 @@ st.subheader('Number of pickups by hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
