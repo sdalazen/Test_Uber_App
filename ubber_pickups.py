8911d7d14db64e42c07c45ff04d4887322ff3aa2
sdalazen <sba22149@student.cct.ie>
2025-05-13 18:59:27 +0100

    Update

--- ubber_pickups.py
+++ ubber_pickups.py
@@ -19,3 +19,18 @@ data_load_state=st.text("Loading Data...")
 data=load_data(10000)
 data_load_state.text("Loading Data...done!")
 
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
